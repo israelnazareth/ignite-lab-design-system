712303c1fe042fb7f81d919d0d6c314f5543e18d
Israel Nazareth <israel.fab@hotmail.com>
2022-10-12 22:03:26 -0300

    feat: cria a aplicação

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,20 @@
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
   
   return (
     <Comp className={clsx(
-      "py-4 px-3 rounded bg-cyan-500 font-semibold text-black transition-colors hover:bg-cyan-300 text-sm w-full focus:ring-2 ring-white"
+      "py-3 px-4 rounded bg-cyan-500 font-semibold text-black transition-colors hover:bg-cyan-300 text-sm w-full focus:ring-2 ring-white", className
       )}
+      {...props}
     >
       {children}
     </Comp>
